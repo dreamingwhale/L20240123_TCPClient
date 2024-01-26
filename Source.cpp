f0743a3623e117c955b35dfd17c4e492259d833b
dreamingwhale <dreamingskywhale@gmail.com>
2024-01-26 15:15:27 +0900

    shhhhhhhh

--- Source.cpp
+++ Source.cpp
@@ -1,83 +1,70 @@
-﻿#include <winsock2.h>
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <winsock2.h>
 #include <windows.h>
 #include <process.h>
-#include <vector>
-#include <set>
-#include <map>
+#include <iostream>
 
 #pragma comment(lib, "ws2_32")
 
-std::set<SOCKET> SessionList;
+SOCKET ServerSocket;
 
-CRITICAL_SECTION SessionCS;
-
-unsigned WINAPI WorkerThread(void* Arg)
+unsigned WINAPI SendThread(void* Arg)
 {
-	SOCKET ClientSocket = *((SOCKET*)Arg);
+	char Message[1024] = { 0, };
+
 	while (true)
 	{
-		char Buffer[1024] = { 0, };
-		int RecvLength = recv(ClientSocket, Buffer, sizeof(Buffer), 0);
-		if (RecvLength <= 0)
-		{
-			EnterCriticalSection(&SessionCS);
-			SessionList.erase(ClientSocket);
-			LeaveCriticalSection(&SessionCS);
-			closesocket(ClientSocket);
-			//ExitThread(-1);
-			break;
-		}
-		else
-		{
-			EnterCriticalSection(&SessionCS);
-			for (auto ConenctSocket : SessionList)
-			{
-				int SendLength = send(ConenctSocket, Buffer, RecvLength, 0);
-			}
-			LeaveCriticalSection(&SessionCS);
-		}
+		std::cin.getline(Message, sizeof(Message));
+
+		int SendLength = send(ServerSocket, Message, (int)strlen(Message), 0);
 	}
+
+
+	return 0;
+}
+
+unsigned WINAPI RecvThread(void* Arg)
+{
+	char Buffer[1024] = { 0, };
+
+	while (true)
+	{
+		int RecvLength = recv(ServerSocket, Buffer, sizeof(Buffer), 0);
+
+		std::cout << Buffer << std::endl;
+	}
+
 	return 0;
 }
 
 int main()
 {
-	InitializeCriticalSection(&SessionCS);
-
-
 	WSAData wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	SOCKET ListenSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	SOCKADDR_IN ListenSockAddr = { 0 , };
-	ListenSockAddr.sin_family = AF_INET;
-	ListenSockAddr.sin_addr.s_addr = INADDR_ANY;// inet_addr("127.0.0.1");
-	ListenSockAddr.sin_port = htons(22222);
+	SOCKADDR_IN ServerSockAddr = { 0 , };
+	ServerSockAddr.sin_family = AF_INET;
+	ServerSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ServerSockAddr.sin_port = htons(22222);
 
-	bind(ListenSocket, (SOCKADDR*)&ListenSockAddr, sizeof(ListenSockAddr));
+	connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
 
-	listen(ListenSocket, 5);
+	HANDLE ThreadHandles[2];
+
+	ThreadHandles[0] = (HANDLE)_beginthreadex(0, 0, SendThread, 0, 0, 0);
+	ThreadHandles[1] = (HANDLE)_beginthreadex(0, 0, RecvThread, 0, 0, 0);
 
 	while (true)
 	{
-		SOCKADDR_IN ClientSockAddr = { 0 , };
-		int ClientSockAddrSize = sizeof(ClientSockAddr);
-		SOCKET ClientSocket = accept(ListenSocket, (SOCKADDR*)&ClientSockAddr, &ClientSockAddrSize);
-		//thread ½ÇÇà, worker thread
-		//CreateThread();
-		HANDLE ThreadHandle = (HANDLE)_beginthreadex(0, 0, WorkerThread, (void*)&ClientSocket, 0, 0);
-		//TerminateThread(ThreadHandle, -1);
-		EnterCriticalSection(&SessionCS);
-		SessionList.insert(ClientSocket);
-		LeaveCriticalSection(&SessionCS);
+		Sleep(1);
 	}
 
-	closesocket(ListenSocket);
+	closesocket(ServerSocket);
 
 	WSACleanup();
-
-	DeleteCriticalSection(&SessionCS);
 
 	return 0;
 }
